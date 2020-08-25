d73e71a88dbc51e914da5b5b426bcc56ac3b56b5
Kelly Elton <its.the.doc@gmail.com>
2020-08-25 17:04:02 -0500

    start

---
+++ src/Data/SqlServer/PageVersions.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PageVersions]
+(
+	[Path] NVARCHAR(2048) NOT NULL ,
+    [Version] INT NOT NULL,
+    [Contents] NTEXT NOT NULL,
+    [Details] NTEXT NOT NULL,
+    [CreatedDate] DATETIMEOFFSET NULL DEFAULT SYSDATETIMEOFFSET(),
+    [CreatedBy] NVARCHAR(128) NOT NULL,
+    CONSTRAINT [PK_PageVersions] PRIMARY KEY ([Path], [Version]),
+    CONSTRAINT [CHK_PageVersions_CreatedByLength] CHECK (LEN([CreatedBy]) > 2),
+    CONSTRAINT [CHK_PageVersions_VersionBounds] CHECK([Version] >= 0)
+);
---
+++ src/Data/SqlServer/Pages.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Pages]
+(
+    [Path] NVARCHAR(2048) NOT NULL,
+    [Version] INT NOT NULL,
+    [Contents] NTEXT NOT NULL,
+    CONSTRAINT [PK_Pages] PRIMARY KEY ([Path]),
+    CONSTRAINT [CHK_Pages_VersionBounds] CHECK([Version] >= 0)
+)
